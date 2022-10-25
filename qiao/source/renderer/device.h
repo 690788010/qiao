8dbc43c7b8369b363099ef530d91b764e4006c2e
liyangqiao <690788010@qq.com>
2022-10-25 18:14:58 +0800

    init

---
+++ qiao/source/renderer/device.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+#include "graphics_window.h"
+
+namespace qiao {
+	class Device {
+	public:
+		static GraphicsWindow* createWindow(int width, int height, std::string title);
+	};
+}
